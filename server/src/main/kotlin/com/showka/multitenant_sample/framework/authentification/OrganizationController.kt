734ecf495ea8765350ab24fb301984e22a76c6a9
sk <user@test.com>
2022-03-24 16:23:04 +0900

    Organization api #register

--- server/src/main/kotlin/com/showka/multitenant_sample/framework/authentification/OrganizationController.kt
+++ server/src/main/kotlin/com/showka/multitenant_sample/framework/authentification/OrganizationController.kt
@@ -1,14 +1,12 @@
 package com.showka.multitenant_sample.framework.authentification
 
 import com.showka.multitenant_sample.system.authentification.OrganizationService
+import com.showka.multitenant_sample.system.value.ID
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/organizations")
@@ -30,7 +28,25 @@ class OrganizationController {
 		}
 	}
 
+	/**
+	 * register new organization.
+	 */
+	@PostMapping
+	@PreAuthorize("isAuthenticated()")
+	@ResponseBody
+	fun register(@AuthenticationPrincipal token: Jwt, @RequestBody form: Form): Response {
+		val id = ID()
+		val orgId = "org" + id.value
+		val organization = service.create(orgId, form.displayName)
+		service.addMember(organization.id, token.subject)
+		return Response(organization.id, organization.identifierName, organization.displayName)
+	}
+
 	companion object {
+		class Form {
+			lateinit var displayName: String
+		}
+
 		class Response(
 			val id: String,
 			val identifierName: String,
