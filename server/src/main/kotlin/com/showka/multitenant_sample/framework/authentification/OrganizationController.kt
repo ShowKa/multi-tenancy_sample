0225412da2bcc7b740a12a5febce40cbf7f936d0
sk <user@test.com>
2022-03-30 14:26:56 +0900

    Organization api: patch

--- server/src/main/kotlin/com/showka/multitenant_sample/framework/authentification/OrganizationController.kt
+++ server/src/main/kotlin/com/showka/multitenant_sample/framework/authentification/OrganizationController.kt
@@ -2,6 +2,7 @@ package com.showka.multitenant_sample.framework.authentification
 
 import com.showka.multitenant_sample.system.authentification.Organization
 import com.showka.multitenant_sample.system.authentification.OrganizationService
+import com.showka.multitenant_sample.system.authentification.auth0.getOrganizationId
 import com.showka.multitenant_sample.system.value.ID
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
@@ -40,6 +41,18 @@ class OrganizationController {
 		val orgId = "org" + id.value
 		val organization = service.create(orgId, form.displayName)
 		service.addMember(organization.id, token.subject)
+		return Response(organization)
+	}
+
+	/**
+	 * update organization name.
+	 */
+	@PatchMapping
+	@PreAuthorize("isAuthenticated()")
+	@ResponseBody
+	fun patch(@AuthenticationPrincipal token: Jwt, @RequestBody form: Form): Response {
+		val orgId = token.getOrganizationId()!!
+		val organization = service.updateDisplayName(orgId, form.displayName)
 		return Response(organization)
 	}
 
