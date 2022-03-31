b00f4eae8b4aad358a6fb806493107e5a16d7ef4
sk <user@test.com>
2022-03-31 16:31:07 +0900

    add invite view in setting view

--- client/src/views/setting/Top.vue
+++ client/src/views/setting/Top.vue
@@ -8,22 +8,29 @@
     </div>
     <label>members</label>
     <MemberList />
+    <a class="setting-top__link" @click="invite">inviete a member</a>
     <EditOrg ref="EditOrgView" />
+    <Invite ref="InviteView" />
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import Organization from '@/models/Organization'
+import Invite from '@/views/invitation/Invite.vue'
 import MemberList from './MemberList.vue'
 import EditOrg from './EditOrg.vue'
 // data
 const orgName = ref('')
 // reffer component
-const EditOrgView = ref('')
+const EditOrgView = ref(null)
+const InviteView = ref(null)
 // method
 const edit = () => {
   EditOrgView.value.open()
+}
+const invite = () => {
+  InviteView.value.open()
 }
 // life cycle event
 onMounted(async () => {
