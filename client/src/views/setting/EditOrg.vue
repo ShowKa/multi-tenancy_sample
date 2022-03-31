e8dee9c74a67afa4ead9a251fe5e0552c95b24c7
sk <user@test.com>
2022-03-31 16:16:27 +0900

    split organization edit view

---
+++ client/src/views/setting/EditOrg.vue
@@ -0,0 +1,40 @@
+<template>
+  <Modal v-if="show" class="org-modal">
+    <template v-slot:header>
+      edit organization
+    </template>
+    <template v-slot:body>
+      <EditOrganization @done="onDone($event)" />
+    </template>
+    <template v-slot:footer>
+      <a class="org-modal__link" @click="show = false">cancel</a>
+    </template>
+  </Modal>
+</template>
+
+<script setup>
+import { defineExpose, ref } from 'vue'
+import Modal from '@/components/modal/Modal.vue'
+import EditOrganization from '@/views/organization/Edit.vue'
+// data
+const show = ref(false)
+// methods
+const onDone = () => {
+  show.value = false
+}
+const open = () => {
+  show.value = true
+}
+// expose
+defineExpose({
+  open
+})
+</script>
+
+<style lang="scss" scoped>
+.org-modal {
+  &__link {
+    @extend %text_link;
+  }
+}
+</style>
--- client/src/views/setting/Top.vue
+++ client/src/views/setting/Top.vue
@@ -4,37 +4,26 @@
     <div>
       <label>organization</label>
       <div>{{ orgName }}</div>
-      <a class="setting-top__link" @click="goEdit = true">edit</a>
+      <a class="setting-top__link" @click="edit">edit</a>
     </div>
     <label>members</label>
     <MemberList />
-    <Modal v-if="goEdit">
-      <template v-slot:header>
-        edit organization
-      </template>
-      <template v-slot:body>
-        <EditOrganization @done="onDone($event)" />
-      </template>
-      <template v-slot:footer>
-        <a class="setting-top__link" @click="goEdit = false">cancel</a>
-      </template>
-    </Modal>
+    <EditOrg ref="EditOrgView" />
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import Organization from '@/models/Organization'
-import Modal from '@/components/modal/Modal.vue'
-import EditOrganization from '@/views/organization/Edit.vue'
 import MemberList from './MemberList.vue'
+import EditOrg from './EditOrg.vue'
 // data
 const orgName = ref('')
-const goEdit = ref(false)
+// reffer component
+const EditOrgView = ref('')
 // method
-const onDone = org => {
-  orgName.value = org.displayName
-  goEdit.value = false
+const edit = () => {
+  EditOrgView.value.open()
 }
 // life cycle event
 onMounted(async () => {
