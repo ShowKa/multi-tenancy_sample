930f54cdaca747d2efa32ba2b48bea4dac30f8c3
sk <user@test.com>
2022-04-01 14:58:09 +0900

    refactoring setting component

--- client/src/views/setting/EditOrg.vue
+++ client/src/views/setting/EditOrg.vue
@@ -1,5 +1,9 @@
 <template>
-  <Modal v-if="show" class="org-modal">
+  <div class="setting-org__form">
+    <div>{{ organization.displayName }}</div>
+    <a class="setting-org__form__link" @click="show = true">edit</a>
+  </div>
+  <Modal v-if="show" class="setting-org__modal">
     <template v-slot:header>
       edit organization
     </template>
@@ -7,34 +11,40 @@
       <EditOrganization @done="onDone" />
     </template>
     <template v-slot:footer>
-      <a class="org-modal__link" @click="show = false">cancel</a>
+      <a class="setting-org__modal__link" @click="show = false">cancel</a>
     </template>
   </Modal>
 </template>
 
 <script setup>
-import { defineExpose, ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
 import Modal from '@/components/modal/Modal.vue'
 import EditOrganization from '@/views/organization/Edit.vue'
+// inject plugin
+const $store = useStore()
 // data
 const show = ref(false)
-// methods
+// computed
+const organization = computed(() => $store.state.organization.current)
+// method
 const onDone = () => {
   show.value = false
 }
-const open = () => {
-  show.value = true
-}
-// expose
-defineExpose({
-  open
-})
 </script>
 
 <style lang="scss" scoped>
-.org-modal {
-  &__link {
-    @extend %text_link;
+.setting-org {
+  &__form {
+    @extend %form;
+    &__link {
+      @extend %text_link;
+    }
+  }
+  &__modal {
+    &__link {
+      @extend %text_link;
+    }
   }
 }
 </style>
--- client/src/views/setting/Top.vue
+++ client/src/views/setting/Top.vue
@@ -1,37 +1,21 @@
 <template>
-  <div class="setting-top">
-    <div class="setting-top__title">Setting and Members</div>
-    <div>
-      <label>organization</label>
-      <div>{{ organization.displayName }}</div>
-      <a class="setting-top__link" @click="edit">edit</a>
-    </div>
+  <div class="setting-top__title">Setting and Members</div>
+  <div class="setting-top__form">
+    <label>organization</label>
+    <EditOrg />
     <label>members</label>
     <MemberList />
     <a class="setting-top__link" @click="invite">inviete a member</a>
-    <!-- popup modal window -->
-    <EditOrg ref="EditOrgView" />
     <Invite ref="InviteView" />
   </div>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
-import { useStore } from 'vuex'
+import { ref } from 'vue'
 import Invite from '@/views/invitation/Invite.vue'
 import MemberList from './MemberList.vue'
 import EditOrg from './EditOrg.vue'
-// inject plugin
-const $store = useStore()
-// ref component
-const EditOrgView = ref(null)
 const InviteView = ref(null)
-// computed
-const organization = computed(() => $store.state.organization.current)
-// method
-const edit = () => {
-  EditOrgView.value.open()
-}
 const invite = () => {
   InviteView.value.open()
 }
@@ -39,9 +23,11 @@ const invite = () => {
 
 <style lang="scss" scoped>
 .setting-top {
-  @extend %form;
   &__title {
     @extend %text_title;
+  }
+  &__form {
+    @extend %form;
   }
   &__link {
     @extend %text_link;
