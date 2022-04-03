c7fec950a8f28f1db6278880932cd57fb3cf6174
sk <user@test.com>
2022-04-03 16:48:20 +0900

    refactoring

--- server/src/main/kotlin/com/showka/multitenant_sample/system/authentification/RoleAssignService.kt
+++ server/src/main/kotlin/com/showka/multitenant_sample/system/authentification/RoleAssignService.kt
@@ -1,10 +1,29 @@
 package com.showka.multitenant_sample.system.authentification
 
 interface RoleAssignService {
-	fun assign(role: Role, userClaim: Claim)
-	fun assign(roleList: List<Role>, userClaim: Claim)
+
 	fun assign(role: Role, userClaimList: List<Claim>)
-	fun assign(roleList: List<Role>, userClaimList: List<Claim>)
+
 	fun assign(roleList: List<Role>, userClaim: Claim, organization: Organization)
-	fun assign(role: Role, userClaim: Claim, organization: Organization)
+
+	// default
+	fun assign(role: Role, userClaim: Claim) {
+		this.assign(role, listOf(userClaim))
+	}
+
+	fun assign(roleList: List<Role>, userClaim: Claim) {
+		roleList.forEach {
+			this.assign(it, userClaim)
+		}
+	}
+
+	fun assign(roleList: List<Role>, userClaimList: List<Claim>) {
+		roleList.forEach {
+			this.assign(it, userClaimList)
+		}
+	}
+
+	fun assign(role: Role, userClaim: Claim, organization: Organization) {
+		this.assign(listOf(role), userClaim, organization)
+	}
 }
