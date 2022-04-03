f16d96f75621d763240d6389259f90815f729f3f
sk <user@test.com>
2022-04-03 17:33:53 +0900

    assign Operator on invite

--- server/src/main/kotlin/com/showka/multitenant_sample/system/authentification/auth0/InvitationServiceImpl.kt
+++ server/src/main/kotlin/com/showka/multitenant_sample/system/authentification/auth0/InvitationServiceImpl.kt
@@ -4,8 +4,8 @@ import com.auth0.client.mgmt.ManagementAPI
 import com.auth0.json.mgmt.organizations.Invitee
 import com.auth0.json.mgmt.organizations.Inviter
 import com.auth0.json.mgmt.organizations.Roles
-import com.showka.multitenant_sample.system.authentification.Invitation
 import com.showka.multitenant_sample.system.authentification.InvitationService
+import com.showka.multitenant_sample.system.authentification.Role
 import com.showka.multitenant_sample.system.authentification.RoleService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -20,47 +20,22 @@ class InvitationServiceImpl : InvitationService {
 	@Autowired
 	private lateinit var roleService: RoleService
 
-	override fun invite(invitation: Invitation): String {
-		// get role id
-		val roleIds = mutableListOf<String>()
-		invitation.roles?.forEach {
-			val id = roleService.getRoleId(it)
-			roleIds.add(id)
-		}
-		// do
-		return this.invite(
-			invitation.clientId, invitation.organization.id,
-			invitation.inviterName, invitation.inviteeMailAddress.string,
-			roleIds
-		)
-	}
-
-	override fun invite(
-		clientId: String,
-		organizationId: String,
-		inviterName: String,
-		inviteeMailAddress: String
-	): String {
-		return this.invite(
-			clientId, organizationId,
-			inviterName, inviteeMailAddress,
-			listOf()
-		)
-	}
-
 	override fun invite(
 		clientId: String, organizationId: String,
 		inviterName: String, inviteeMailAddress: String,
-		roleIds: List<String>
+		roleList: List<Role>
 	): String {
 		// invitation for auth0
 		val inviter = Inviter(inviterName)
 		val invitee = Invitee(inviteeMailAddress)
 		val auth0Invitation = Auth0_Invitation(inviter, invitee, clientId)
-		// role
-		if (roleIds.isNotEmpty()) {
-			auth0Invitation.roles = Roles(roleIds)
+		// get role id
+		val roleIds = mutableListOf<String>()
+		roleList.forEach {
+			val id = roleService.getRoleId(it)
+			roleIds.add(id)
 		}
+		auth0Invitation.roles = Roles(roleIds)
 		val result =
 			managementApi.organizations().createInvitation(organizationId, auth0Invitation).execute()
 		return result.id
