d36ea66f843b78bb59a1fc429e2c2bec42316a8f
sk <user@test.com>
2022-04-05 14:18:52 +0900

    remove unnecessary

--- server/src/main/kotlin/com/showka/multitenant_sample/system/tenant/IllegalTenantAccessException.kt
+++
@@ -1,7 +0,0 @@
-package com.showka.multitenant_sample.system.tenant
-
-import com.showka.multitenant_sample.system.exception.SystemException
-
-class IllegalTenantAccessException() :
-	SystemException("不正なテナントにアクセス") {
-}
